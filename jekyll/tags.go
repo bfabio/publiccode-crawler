d34f16c5814168922d64ece581e3bdc215ef5e05
Fabio Bonelli <fb@fabiobonelli.it>
2022-04-21 18:07:58 +0200

    refactor: move the code to the root directory

--- jekyll/tags.go
+++ jekyll/tags.go
@@ -5,7 +5,7 @@ import (
 	"os"
 
 	"github.com/ghodss/yaml"
-	"github.com/italia/developers-italia-backend/crawler/elastic"
+	"github.com/italia/developers-italia-backend/elastic"
 	es "github.com/olivere/elastic/v7"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
