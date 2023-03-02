effae51685e8aa31ecd9ee9951cca0a0998a073c
Fabio Bonelli <fb@fabiobonelli.it>
2023-03-02 19:19:44 +0100

    Revert "Revert "feat: clone bare repositories without blobs (#269)""
    This reverts commit fda193fe889bdd1df5fec63f1fff5bc46f0a1ae7.

--- git/clone_repository.go
+++ git/clone_repository.go
@@ -34,9 +34,7 @@ func CloneRepository(hostname, name, gitURL, index string) error {
 		return nil
 	}
 
-	// Clone the repository using the external command "git".
-	// Command is: git clone -b <branch> <remote_repo>
-	out, err := exec.Command("git", "clone", gitURL, path).CombinedOutput() // nolint: gas
+	out, err := exec.Command("git", "clone", "--filter=blob:none", "--mirror", gitURL, path).CombinedOutput()
 	if err != nil {
 		return fmt.Errorf("cannot git clone the repository: %s: %s", err.Error(), out)
 	}
